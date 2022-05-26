e8c81d25705861253322e3746b544801de9acfac
tohe0133 <105419217+tohe0133@users.noreply.github.com>
2022-05-26 12:12:46 +0200

    Done?

--- areYouBored_controller.py
+++ areYouBored_controller.py
@@ -3,39 +3,54 @@ from areYouBored_model import get_restaurant, go_to_restaurant, id_generation, s
 
 app = Flask(__name__)
 restaurant_id = 0
-user_location = ()
+
+distance = 2000
+rating = 1
+price = 3
+is_sorted = False
 
 
 @app.route('/')
 def home():
-    global restaurant_id
+    global restaurant_id, is_sorted
+    is_sorted = False
     restaurant_id = id_generation()
     return render_template('index.html', message=None)
 
 
 @app.route('/print')
 def print():
-    global restaurant_id
-    restaurant_id = id_generation()
-    get_restaurant(restaurant_id)
-    return render_template('view.html', restaurants=get_restaurant(restaurant_id))
-
-
-@app.route('/sorted_print')
-def sorted_print():
-    global restaurant_id
-    get_restaurant(restaurant_id)
-    return render_template('view.html', restaurants=get_restaurant(restaurant_id))
+    global restaurant_id, is_sorted, distance, rating, price
+    if is_sorted == False:
+        restaurant_id = id_generation()
+        get_restaurant(restaurant_id)
+        return render_template('view.html', restaurants=get_restaurant(restaurant_id))
+    else:
+        restaurant_id = sorting(distance, rating, price)
+        if restaurant_id == 0:
+            return render_template('index.html', message='Could not find any restaurant. Try again')
+        else:
+            get_restaurant(restaurant_id)
+            return render_template('view.html', restaurants=get_restaurant(restaurant_id))
 
 
 @app.route('/sort', methods=['POST'])
 def sort_rest():
-    global restaurant_id
-    restaurant_id = sorting(int(request.values.get('distance')), int(request.values.get('rate')),int(request.values.get('price')))
-    if restaurant_id == 0:
-        return render_template('index.html', message='Could not find any restaurant. Try again')
-    else:
-        return render_template('index.html', message='Sorting restaurants...')
+    global distance, rate, price, is_sorted
+    try:
+        distance = int(request.values.get('distance'))
+    except TypeError:
+        pass
+    try:
+        rate = int(request.values.get('rate'))
+    except TypeError:
+        pass
+    try:
+        price = int(request.values.get('price'))
+    except TypeError:
+        pass
+    is_sorted = True
+    return render_template('index.html', message='Sorting restaurants...')
 
 
 @app.route('/take_me_there')
